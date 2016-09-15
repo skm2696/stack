c0e4237cd5d5859a912824facac65dd8e9399098
skm2696 <skm03092014@gmail.com>
2016-09-15 21:59:16 +0300

    Update Stack.hpp

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -17,9 +17,7 @@ public:
 	~stack();
 	stack(const stack & obj);
 	stack & operator=(const stack &obj);
-	//void swap(stack & obj);
 	bool operator==(stack const & rhs);
-	//friend ostream & operator<< <>(ostream & output, stack<T> const & stack);
 private:
 	T * array_;
 	size_t array_size_;
