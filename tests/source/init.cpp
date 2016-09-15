85e8bd633aed54d960d58ae03dc9b97bb1f98d05
skm2696 <skm03092014@gmail.com>
2016-09-15 21:36:03 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -17,11 +17,11 @@ SCENARIO("Stack: operator=", "[op=]") {
 	b = a;
 	REQUIRE(b == a);
 }
-SCENARIO("Stack: operator=", "[op=]") {
+SCENARIO("Stack: count", "[op=]") {
 	stack<int> a;
 	a.push(7);
 	a.push(6);
 	a.push(8);
 	a.push(9);
-	REQUIRE(a.count() == 9);
+	REQUIRE(a.count() == 4);
 }
