d8e003bf2c5b494117176ba60c7457b8d049d8b2
skm2696 <skm03092014@gmail.com>
2016-09-18 22:52:13 +0300

    Update Stack.cpp

--- include/Stack.cpp
+++ include/Stack.cpp
@@ -63,7 +63,7 @@ stack<T>::~stack()
 }
 
 template <typename T>//конструктор копирования
-stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(copy(tmp.count_, tmp.array_size, tmp.array_)) {};
+stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(copy(tmp.count_, tmp.array_size_, tmp.array_)) {};
 template <typename T>
 stack<T>& stack<T>::operator=(const stack &obj) 
 {
