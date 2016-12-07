177ff1a269afc58dec1004e531074f0c40ac30e0
skm2696 <skm03092014@gmail.com>
2016-12-07 16:26:30 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,53 +1,62 @@
 #include "Stack.hpp"
 #include <catch.hpp>
+#include <iostream>
+#include <thread>
+#include<mutex>
+using namespace std;
+ 
 SCENARIO("count", "[count]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
 }
 
-SCENARIO("top", "[top]"){
+SCENARIO("push", "[push]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
+}
+
+SCENARIO("pop", "[pop]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(*(s.pop())==1);	
+  REQUIRE(s.count()==0);
+}
+
+SCENARIO("prisv", "[prisv]"){
+  stack<int> s;
+  s.push(1);
+  stack<int> s2;
+  s2=s;
+  REQUIRE(s.count()==1);
+}
+
+SCENARIO("empty", "[empty]"){
+  stack<int> s1, s2;
+  s1.push(1);
+  REQUIRE(!s1.empty());
+  REQUIRE(s2.empty());
+}
+
+
+SCENARIO("threads", "[threads]"){
   stack<int> s;
   s.push(1);
   s.push(2);
   s.push(3);
-  s.pop();
-  REQUIRE(s.top()==2);
-}
-
-
-SCENARIO("empty", "[empty]"){
-  stack<int> s1;
-  s1.push(1);
-  REQUIRE(s1.empty()==false);
-}
-
-SCENARIO("empty2", "[empty2]"){
-  stack<int> s1;
-  s1.push(1);
-  s1.pop();
-  REQUIRE(s1.empty()==true);
-}
-
-SCENARIO("empty3", "[empty3]"){
-  stack<int> s1;
-  s1.push(1);
-  s1.push(2);
-  s1.pop();
-  s1.top();
-  
-  REQUIRE(s1.empty()==false);
-}
-
-SCENARIO("mutex", "[mutex]"){
-  stack<int> s1;
-  s1.push(1);
-  s1.push(2);
-  
-  thread potok_1(&stack<int>::push, &s1, 3);
-  potok_1.join();
-  REQUIRE(s1.top()==3);
-  thread potok_2(&stack<int>::pop, &s1);
-  potok_2.join();
-  REQUIRE(s1.top()==2);
+	std::thread t1([&s](){
+		for (int i = 0; i < 5; i++) {
+			s.push(i + 4);
+		}
+	});
+	std::thread t2([&s](){
+		for (int i = 0; i < 5; i++)
+		{
+			s.pop();
+		}
+	});
+	t1.join();
+	t2.join();
+  REQUIRE(s.count()==3);
 }
